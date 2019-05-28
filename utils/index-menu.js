50758b4bd4da087506624511413e482d197a084b
Chris Pyles <cpyles@berkeley.edu>
2019-05-28 13:19:04 -0700

    added more information about the project

--- utils/index-menu.js
+++ utils/index-menu.js
@@ -1,4 +1,4 @@
 var html = `
-	<strong>Notebooks:</strong> <a href="index.html">Home</a> | <a href="export/01-loading-data.html">Loading Data</a><br><br>
+	<strong>Notebooks:</strong> <a href="index.html">Home</a> | <a href="export/01-sample-page.html">Sample Page</a><br><br>
 `;
 document.write(html)
--- utils/menu.js
+++ utils/menu.js
@@ -1,4 +1,4 @@
 var html = `
-	<strong>Notebooks:</strong> <a href="../index.html">Home</a> | <a href="01-loading-data.html">Loading Data</a>
+	<strong>Notebooks:</strong> <a href="../index.html">Home</a> | <a href="01-sample-page.html">Sample Page</a>
 `;
 document.write(html)
